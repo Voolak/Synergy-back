e4fa459ae17f56b24a98c5a46adde2efd8ad3719
Voolak <abarzana.perso@gmail.com>
2024-02-05 14:56:39 +0100

    Getting Started Spring Initializer

---
+++ src/main/java/com/slack/synergy/SynergyApplication.java
@@ -0,0 +1,13 @@
+package com.slack.synergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slack/synergy/SynergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slack.synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
