26f9645d5b5afb170fcf73311a0d857b035a3b26
Boryana Baeva <b.baeva@labom2iformation.fr>
2024-02-06 13:46:28 +0100

    created entity Message

---
+++ src/main/java/com/slack/synergy/model/Message.java
@@ -0,0 +1,90 @@
+package com.slack.synergy.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="message_type",
+        discriminatorType = DiscriminatorType.STRING)
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+    private int upvote;
+    private int downvote;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate = LocalDateTime.now();
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+
+    public Message() {
+    }
+
+    public Message(String content, User sender) {
+        this.content = content;
+        this.sender = sender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getUpvote() {
+        return upvote;
+    }
+
+    public void setUpvote(int upvote) {
+        this.upvote = upvote;
+    }
+
+    public int getDownvote() {
+        return downvote;
+    }
+
+    public void setDownvote(int downvote) {
+        this.downvote = downvote;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
---
+++ src/main/java/com/slack/synergy/model/PrivateMessage.java
@@ -0,0 +1,7 @@
+package com.slack.synergy.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class PrivateMessage {
+}
